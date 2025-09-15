6f7b62843a1eaa1dc2adc09e5b7d2f6823787715
Olga Samsonova <medovichek@gmail.com>
2025-09-15 10:22:37 +0300

    Дополнены изменение метода main, для расчетов суммы, разности, произведения и частноя. Добавлена возможность введения данных в консоли.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int firstNumber = 5;
-        int secondNumber = 10;
-        int sum = firstNumber + secondNumber;
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+        int sum = number1 + number2;
         System.out.println("Сумма: " + sum);
-        int difference = firstNumber - secondNumber;
+        int difference = number1 - number2;
         System.out.println("Разность: " + difference);
-        int product = firstNumber * secondNumber;
+        int product = number1 * number2;
         System.out.println("Произведение: " + product);
-        double quotient = (double) firstNumber / secondNumber;
+        double quotient = (double) number1 / number2;
         System.out.println("Частное: " + quotient);
     }
 }
